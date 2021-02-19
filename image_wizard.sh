ec50ee37d2f5eec5b74032b189dc2487fb8df56f
Michael Farber Brodsky <taco@mailo.com>
2021-02-19 12:57:25 +0200

    Fix image wizard

--- image_wizard.sh
+++ image_wizard.sh
@@ -2,7 +2,7 @@ echo Image Wizard
 
 read -r -p "image size (default 32G, not allocated): " image_size
 if [ -z "$image_size" ]; then
-    $image_size=32G
+    image_size=32G
 fi
 
 qemu-img create -f qcow2 image.qcow2 $image_size
