ff2e55e973818908dd4f9439273da152cebc9936
Michael Farber Brodsky <taco@mailo.com>
2021-02-23 21:08:08 +0200

    Add update_main.sh, which runs a set command to update the vm's software.

--- config.sh
+++ config.sh
@@ -1,2 +1,3 @@
 MEMORY=2048M
 CPU_CORES=2
+UPDATE_CMD="sudo apt update && sudo apt upgrade && sudo poweroff"
---
+++ update_main.sh
@@ -0,0 +1,18 @@
+source config.sh
+source ./util/get_port.sh
+
+my_img="${2:-image.qcow2}"  # default to image.qcow2
+qemu-kvm -m $MEMORY -nographic -smp $CPU_CORES -drive file=$my_img,format=qcow2 \
+-device e1000,netdev=net0 \
+-netdev user,id=net0,hostfwd=tcp::$port-:22 > /dev/null &
+qemu_pid=$!
+
+# try to ssh until it connects
+while true
+do
+    ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=1 localhost -p $port "$UPDATE_CMD"
+    # if the server closes, stop
+    if ! (stat /proc/$qemu_pid/cmdline >/dev/null 2>/dev/null); then
+        exit
+    fi
+done
